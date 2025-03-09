69f6ba6be8f0e996a6c39edb0ff8716df05ec27a
Aditya Banka <adityabanka1947@gmail.com>
2025-03-09 16:22:05 +0530

    better project section header

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -4,7 +4,7 @@ import ProjectSection from '../Components/ProjectSection'
 
 
 function Project() {
-  
+
   const [count, setCount] = useState(10);
   const handleClick = () => {
     setCount(count + 1);
@@ -12,12 +12,14 @@ function Project() {
 
   return (
     <div className='flex flex-col pt-20 lg:pt-60 bg-[#f5f5f7] w-full space-y-50 pb-50'>
+
       <div className='flex flex-col justify-center items-center space-y-10 mx-[10%]'>
         <p className='text-7xl font-bold'>
-          My <span className="text-red-700 active:scale-95 inline-block duration-50 select-none" onClick={handleClick}>{count}</span>x Developer <span className='whitespace-nowrap'><span className='text-red-700 active:scale-95 inline-block duration-50 select-none' onClick={handleClick}>Pro</span>jects.</span> {count === 69? "😏Nice!" : ""}
+          My <span className="text-red-700 active:scale-95 inline-block duration-50 select-none" onClick={handleClick}>{count}</span>x Developer <span className='whitespace-nowrap'><span className='text-red-700 active:scale-95 inline-block duration-50 select-none' onClick={handleClick}>Pro</span>jects.</span> {count === 69 ? "😏Nice!" : ""}
         </p>
-        <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloribus non voluptatibus laboriosam blanditiis minus quia laudantium assumenda placeat tenetur atque nihil fugit debitis magni rem accusamus, molestiae vel sint soluta molestias? Nulla, incidunt libero impedit quasi ipsam explicabo repellat doloribus, voluptatum soluta eaque fugiat labore placeat! Sed blanditiis eligendi incidunt iusto earum similique voluptatem vel est! Voluptate id dicta harum unde magni non reprehenderit labore, voluptates atque, dignissimos autem similique. Ex mollitia ipsa, itaque sequi quas veniam laborum ad facilis, quo vero illo, repudiandae expedita provident. Neque quasi, nobis fugit incidunt suscipit, maxime vitae corporis fuga ullam id aut.</p>
+        <p className='text-justify'>I have been programming since the age of 13. Since then, I have worked with Java, C, C++, Python, JavaScript, HTML, CSS, and many other programming languages. In my journey I have made myself well versed in tools such as Git, Docker, VSCode, Postman, Android Studio, Chrome, Arduino, STM32, along with frameworks and libraries such as ReactJs, Tailwind, Spring, NumPy, Pandas, SciKit-Learn, TensorFlow, Keras, OpenCV, Google MediaPipe and more. I have used popular databases like, MySQL, SQLite, and MongoB. I frequently use cloud services such as, Amazon Web Services (Lambda, EC2, S2, SES) and Google Cloud Platform as well.</p>
       </div>
+
       <ProjectSection
         title={
           <p>Comparative training of Artificial Neural Network using Genetic and Stocastic Algorithms</p>
@@ -28,12 +30,21 @@ function Project() {
           </p>
         }
       />
-      <ProjectSection direction={0} />
-      <ProjectSection />
-      <ProjectSection direction={0} />
-      <ProjectSection />
-      <ProjectSection direction={0} />
-      <ProjectSection />
+      <ProjectSection
+        direction={0}
+      />
+      <ProjectSection
+      />
+      <ProjectSection
+        direction={0}
+      />
+      <ProjectSection
+      />
+      <ProjectSection
+        direction={0}
+      />
+      <ProjectSection
+      />
     </div>
   )
 }
