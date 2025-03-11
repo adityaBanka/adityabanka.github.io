5ef8785fec49bb93eb3fb9e8f3d7b65421f2d7bc
Aditya Banka <adityabanka1947@gmail.com>
2025-03-12 00:51:23 +0530

    highlighted work page contents

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,9 +6,9 @@ function HomePage() {
 
       <div className='flex lg:flex-row flex-col items-center justify-center'>
         <img src="AdityaBanka.jpg" className='w-1/2 lg:w-1/4 h-auto rounded-[100px] m-5 pointer-events-none select-none' alt="Aditya Banka" />
-        <div className='flex flex-col not-lg:items-center lg:items-left justify-center space-y-1'>
-          <h1 className='text-5xl font-bold text-sky-600'>Aditya Banka</h1>
-          <p>Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Photographer</p>  
+        <div className='flex flex-col not-lg:items-center lg:items-left justify-center lg:space-y-2 space-y-1'>
+          <h1 className='text-4xl lg:text-7xl font-bold text-sky-600'>Aditya Banka</h1>
+          <p className='lg:text-2xl'>Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Photographer</p>  
         </div>
       </div>
 
