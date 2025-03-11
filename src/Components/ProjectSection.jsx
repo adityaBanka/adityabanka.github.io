9526d1f8e38eafee93745570ab6d01fad8b3794c
Aditya Banka <adityabanka1947@gmail.com>
2025-03-11 15:36:15 +0530

    aesthetic overhaul of project page

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -1,7 +1,7 @@
 function ProjectSection({ direction, title = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, nam.", description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At esse possimus inventore repudiandae, sint optio voluptas in impedit reiciendis consequatur, iusto enim architecto doloribus mollitia perferendis facilis veritatis repellendus suscipit dolorum provident qui odio. Inventore veniam quo facere cumque fugit impedit, ad deleniti. Possimus, reprehenderit earum deserunt sit omnis nisi." }) {
   return (
 
-    <div className={`w-full flex justify-center items-center px-[5%] flex-col ${direction === 0 ? "lg:flex-row-reverse lg:space-x-reverse" : "lg:flex-row "}`}>
+    <div className={`flex justify-center items-center p-[1%] flex-col ${direction === 0 ? "lg:flex-row-reverse lg:space-x-reverse" : "lg:flex-row "} bg-white/50 backdrop-blur-2xl rounded-4xl mx-[5%] drop-shadow-xl`}>
 
       <iframe className='min-w-[55%] w-full aspect-16/9 rounded-2xl lg:rounded-4xl pointer-events-none' src="https://www.youtube.com/embed/p2AWYanIHkc?autoplay=1&mute=1&fs=0&controls=0&disablekb=1&rel=0&button=0&loop=1&playlist=p2AWYanIHkc"></iframe>
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import ProjectSection from '../Components/ProjectSection'
 
 function HomePage() {
   return (
-    <div className='flex flex-col justify-center items-center bg-[#f5f5f7] h-full pt-20'>
+    <div className='flex flex-col justify-center items-center bg-gray-100 h-full min-h-screen'>
 
       <div className='flex lg:flex-row flex-col items-center justify-center'>
         <img src="AdityaBanka.jpg" className='w-1/2 lg:w-1/4 h-auto rounded-full m-5 pointer-events-none select-none' alt="Aditya Banka" />
