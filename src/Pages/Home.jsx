b822700a442e8d0138d81113299dfd371746be4f
Aditya Banka <adityabanka1947@gmail.com>
2025-03-11 14:22:40 +0530

    minor homepage improvements

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
 import ProjectSection from '../Components/ProjectSection'
 
 function HomePage() {
   return (
-    <div className='flex flex-col justify-center items-center bg-[#f5f5f7] h-fit pt-20 p-[10%]'>
+    <div className='flex flex-col justify-center items-center bg-[#f5f5f7] h-full pt-20'>
 
       <div className='flex lg:flex-row flex-col items-center justify-center'>
-        <img src="image.png" />
-        <div>
-          <h1 className=''>Aditya Banka</h1>
-          <p>Hi how you doin'?</p>
+        <img src="AdityaBanka.jpg" className='w-1/2 lg:w-1/4 h-auto rounded-full m-5 pointer-events-none select-none' alt="Aditya Banka" />
+        <div className='flex flex-col not-lg:items-center lg:items-left justify-center space-y-1'>
+          <h1 className='text-5xl font-bold text-sky-600'>Aditya Banka</h1>
+          <p>Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Photographer</p>  
         </div>
       </div>
-
-      <ProjectSection></ProjectSection>
-
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import App from './App.jsx'
-import NotFound from "./Pages/NotFound.jsx";
 import './main.css'
 
 
