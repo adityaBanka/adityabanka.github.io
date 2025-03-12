60e1186c07ab037fdf8cf4ee2ac76829b3be6179
Aditya Banka <adityabanka1947@gmail.com>
2025-03-12 20:13:52 +0530

    image size

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function HomePage() {
     <div className='flex flex-col justify-center items-center bg-gray-100 h-full min-h-screen py-20 space-y-10'>
 
       <div className='flex lg:flex-row flex-col items-center justify-center'>
-        <img src="AdityaBanka.jpg" className='w-1/2 lg:w-1/4 h-auto rounded-[100px] m-5 pointer-events-none select-none' alt="Aditya Banka" />
+        <img src="AdityaBanka.jpg" className='w-2/3 lg:w-1/4 h-auto rounded-[100px] m-5 pointer-events-none select-none' alt="Aditya Banka" />
         <div className='flex flex-col not-lg:items-center lg:items-left justify-center lg:space-y-2 space-y-1'>
           <h1 className='text-4xl lg:text-7xl font-bold text-sky-600'>Aditya Banka</h1>
           <p className='lg:text-2xl'>Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Learner</p>
