2efb66713c92cc98b9b8b2ab8675bdf900769987
Aditya Banka <adityabanka1947@gmail.com>
2025-03-13 18:07:12 +0530

    Hello world changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function HomePage() {
   return (
     <div className='flex flex-col bg-gray-100 w-full h-full min-h-screen py-20 lg:pt-60 space-y-20 md:space-y-50'>
       <div className="flex flex-col justify-center items-center space-y-5 mx-[10%]">
-        <h1 className="text-7xl font-bold drop-shadow-2xl">Hello world, I&apos;m <span className="text-blue-500">Aditya Banka</span></h1>
+        <h1 className="text-7xl font-bold drop-shadow-2xl"><span className="text-blue-500">Hello</span> world, I&apos;m <span className="text-blue-500">Aditya</span> Banka</h1>
         <p className="lg:text-justify">Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Founder</p>
       </div>
 
