66dab04e05d9d263342cbad7fac946fb6f8bab55
Aditya Banka <adityabanka1947@gmail.com>
2025-03-14 22:59:14 +0530

    better code quality and imporoved readability

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,25 +8,37 @@ function BW({ children }) {
 }
 
 function HomePage() {
+
+  const infoData = [
+    {
+      title: <p><span className="mb-5 text-3xl font-semibold text-gray-800">&quot;He&apos;s the <span className="text-green-600 text-4xl">Jack</span> of all, and a <span className="text-green-600 text-5xl">Master</span> of many..&quot;<span className="text-xl text-nowrap"> - My dad</span> </span></p>,
+      description: <p className="text-gray-700">From a very young age I have been a man of science. Always wanted to be an <BW>engineer</BW> and now I am one. I am a <BW>quick learner</BW> with a special emphasis on getting behind the why of things. I am <BW>passionate</BW> about all things tech and constantly <BW>strive to expand</BW> the limits of my knowledge. I have explored a ton of <BW>different domains</BW> and have developed a very <BW>robust problem solving</BW> background. Over the years this has led me to be more <BW>efficient</BW> and quick to provide <BW>real-world insights</BW> into engineering complete solutions.</p>,
+      type: "image",
+      URL: "AdityaBanka.jpg"
+    }
+  ]
+
   return (
     <div className='flex flex-col bg-gray-100 w-full h-full min-h-screen py-20 md:pt-60 space-y-20 lg:space-y-50'>
       <div className="flex flex-col justify-center items-center space-y-5 mx-[10%]">
         <h1 className="text-7xl font-bold drop-shadow-2xl"><span className="text-blue-500">Hello</span> world, I&apos;m <span className="text-blue-500">Aditya</span> Banka</h1>
         <p className="lg:text-justify">Creator | Developer | <span className='text-red-700 font-semibold'>Engineer</span> | Founder</p>
       </div>
-      
-      <InfoSection
-      type="image"
-      URL = "AdityaBanka.jpg"
-      title={
-        <span className="mb-5 text-3xl font-semibold text-gray-800">&quot;He&apos;s the <span className="text-green-600 text-4xl">Jack</span> of all, and a <span className="text-green-600 text-5xl">Master</span> of many..&quot;<span className="text-xl text-nowrap"> - My dad</span> </span>
-      }
-      description={
-        <p className="text-gray-700">From a very young age I have been a man of science. Always wanted to be an <BW>engineer</BW> and now I am one. I am a <BW>quick learner</BW> with a special emphasis on getting behind the why of things. I am <BW>passionate</BW> about all things tech and constantly <BW>strive to expand</BW> the limits of my knowledge. I have explored a ton of <BW>different domains</BW> and have developed a very <BW>robust problem solving</BW> background. Over the years this has led me to be more <BW>efficient</BW> and quick to provide <BW>real-world insights</BW> into engineering complete solutions.</p>
-      }
-      />
 
-
+      <div className='space-y-30'>
+        {
+          infoData.map((data, index) => (
+            <InfoSection
+              key={index}
+              direction={index % 2}
+              title={data.title}
+              description={data.description}
+              type={data.type}
+              URL={data.URL}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
