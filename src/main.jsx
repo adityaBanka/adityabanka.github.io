0b9df5154d426eaa980bb5cc0fae93205b1657e3
Aditya Banka <adityabanka1947@gmail.com>
2025-04-19 02:18:12 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './main.css'
-
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Analytics />
     <App></App>
   </BrowserRouter>
 );
